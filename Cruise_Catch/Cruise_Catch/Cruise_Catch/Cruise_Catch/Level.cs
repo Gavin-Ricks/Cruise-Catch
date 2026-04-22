ecbb4a7d61e2d0e8124a6af8bc6cb801ba26281f
Justin-Marx <decamark3@gmail.com>
2026-04-22 15:42:52 -0500

    Update Level.cs with level loader
    BAREBONES, NEEDS FURTHER CHANGES INTO MAIN

--- Cruise_Catch/Cruise_Catch/Cruise_Catch/Cruise_Catch/Level.cs
+++ Cruise_Catch/Cruise_Catch/Cruise_Catch/Cruise_Catch/Level.cs
@@ -40,5 +40,74 @@ namespace Project
             }
             return false;
         }
+        //im using texture block and texture spike and others as a placeholder right now, when the game proceedes maybe you can make the textures of the objects public so i can refernce them here
+        //using two seperate arrays for the blocks and the spikes, in the future add crabs beachballs etc.
+        private void loadLevel(string path,Texture2D block, Texture2D spike)
+        {
+            try
+            {
+                using(StreamReader reader = new StreamReader (path))
+                {
+                    for(int i=0; i<3; i++)
+                    {
+                        if(i == 0)
+                        {
+                            string line = reader.ReadLine();
+                            string[] parts = line.Split(' ');
+                            startPos = new Vector2(Convert.ToInt32(parts[0]), Convert.ToInt32(parts[1]));
+                        }
+                        else if(i == 1)
+                        {
+                            string line = reader.ReadLine();
+                            string[] parts = line.Split(' ');
+                            minimumX = Convert.ToInt32(parts[0]);
+                            minimumY = Convert.ToInt32(parts[1]);
+                        }
+                        else if(i == 2)
+                        {
+                            string line = reader.ReadLine();
+                            string[] parts = line.Split(' ');
+                            maximumX = Convert.ToInt32(parts[0]);
+                            maximumX = Convert.ToInt32(parts[1]);
+                        }
+                    }
+                    for(int i=0; !reader.EndOfStream; i++)
+                    {
+                        string line = reader.ReadLine();
+                        for(int j = 0; i<line.Length; i++)
+                        {
+                            switch(line[j])
+                            {
+                                case 'a':
+                                    blocks[i, j] = null;
+                                    break;
+                                case 'b':
+                                    //TEX WILL BE ASSIGNED TEXTURE WHEN I GAIN ACSESS
+                                    Texture2D tex = null;
+                                    Boolean breakable = true;
+                                    tex = block;
+                                    //here to change block size
+                                    Rectangle pos = new Rectangle(j*50,i*50,50,50);
+                                    blocks[i, j] = new Blocks(tex, pos, breakable);
+                                    break;
+                                case 's':
+                                    Texture2D tex2 = null;
+                                    //this will inherintly make the spikes smaller but have the same distance between them as blocks.
+                                    Rectangle pos2 = new Rectangle(j * 50, i * 50, 20, 20);
+                                    spikes[i, j] = new Spikes(tex2, pos2);
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+                catch (Exception e)
+                {
+                    Console.WriteLine("The file could not read:");
+                    Console.WriteLine(e.Message);
+                }
+        }
+    }
+}
     }
 }
